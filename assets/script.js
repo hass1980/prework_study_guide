45ab35898656397d36c326e2e1f65775002c599d
Hassan Ruby <hassruby@hotmail.co.uk>
2023-09-19 19:59:59 +0100

    Added FOR loop

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+let topics = ["HTML", "CSS", "GIT", "Javascript"];
+
+
+for (let x = 0; topics.length; x++) {
+
+    console.log(topics[x]);
+
+
+}
+
+/*
+
+if (topic === 'HTML') {
+    console.log('Lets study HTML');
+}
+else if (topic === 'CSS') {
+    console.log('Lets learn CSS');
+}
+else if (topic === 'Git') {
+    console.log('Lets study Git');
+}
+else if (topic === 'Javascript') {
+    console.log('Lets study Javascript');
+} else {
+    console.log('Please try again!');
+}
+
+*/
